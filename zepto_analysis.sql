8b74a375d7a9ac5b0562469a90f6aa2db0f1bfc6
vadlasreeja22 <vadlasreeja22@gmail.com>
2025-09-14 16:02:33 +0530

    zepto_analysis.sql

--- zepto_analysis.sql
+++ zepto_analysis.sql
@@ -109,7 +109,7 @@ ORDER BY mrp DESC, discountPercent DESC;
 
 -- Q5. Top 5 categories with highest avg discount
 SELECT category,
-       ROUND(AVG(discountPercent), 2) AS avg_discount
+       ROUND(AVG(discountPercent)) AS avg_discount
 FROM zepto
 GROUP BY category
 ORDER BY avg_discount DESC
